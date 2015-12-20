f8a1116a5d5f2991aa0de96cff7d18b7a12e1263
Chris Hamons <chris.hamons@xamarin.com>
2015-12-19 23:12:22 -0600

    More tool changes

--- tools/walk_dir_print_excluded_files.py
+++ tools/walk_dir_print_excluded_files.py
@@ -8,9 +8,18 @@ with open("TILES2.TXT") as f:
     for line in f:
     	bad_names.add (line.rstrip ())
 
+bad_count = 0
+good_count = 0
 path = "/Users/donblas/Programming/crawl/crawl-ref/source/rltiles"
 for root, dirs, files in os.walk(path):
 	path = root.split('/')
 	for file in files:
 		if file in bad_names:
 			print str(root) + "/" + file
+			bad_count += 1
+		else:
+			good_count += 1
+
+print "Good: " + str(good_count)
+print "Bad: " + str(bad_count)
+
